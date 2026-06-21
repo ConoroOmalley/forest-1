3140b81d42f092b870cf70870a4df2850cc4b640
s0225d0729-ship-it <41600987+ConoroOmalley@users.noreply.github.com>
2026-06-21 14:08:20 +0800

    增加网页访问数字

--- src/components/SiteFooter.vue
+++ src/components/SiteFooter.vue
@@ -1,5 +1,16 @@
 <script setup lang="ts">
+import { onMounted } from 'vue'
 import { blogConfig } from '@/data/posts'
+
+onMounted(() => {
+  if (document.getElementById('busuanzi-script')) return
+
+  const script = document.createElement('script')
+  script.id = 'busuanzi-script'
+  script.async = true
+  script.src = 'https://busuanzi.ibruce.info/busuanzi/2.3/busuanzi.pure.mini.js'
+  document.body.appendChild(script)
+})
 </script>
 
 <template>
@@ -8,5 +19,8 @@ import { blogConfig } from '@/data/posts'
       <span>Designed and coded with care.</span>
       <span>© {{ new Date().getFullYear() }} {{ blogConfig.label }}</span>
     </div>
+    <p id="busuanzi_container_site_pv" class="site-footer-pv">
+      本站总访问量<span id="busuanzi_value_site_pv"></span>次
+    </p>
   </footer>
 </template>
